87af6a9dcb3070ec8d155a2dc49fee5afc419591
Zarivate <Zarivate1@gmail.com>
2023-03-20 03:02:16 -0500

    Implemented shifting linked lists solution and example

--- LinkedLists/Shifting_linked_lists.py
+++ LinkedLists/Shifting_linked_lists.py
@@ -57,6 +57,88 @@
 # k = 30
 # length = 6
 # 15 % 6 = 3 (6 goes into 15 2 times for 12, 15 - 12 = 3)
+# new k = 3
+
+# *****************************
+# If k = -Number
+# Formula changes, goes from new k = k % length to
+# new k/new tail position = k, (just make it positive) IE:
+# k = -2 would become k = 2
+# 0 --> 1 --> 2 --> 3 --> 4 --> 5
+# 2 % 6 = 2, value at 2nd position is 1 which would be the new tail
+# would become
+# 2 --> 3 --> 4 --> 5 --> 0 --> 1
+
+# Optimal Solution:
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+# Using the input example for some comment notes
+def shiftLinkedList(head, k):
+    # Variable to store input string's length for later
+    length = 1
+    # Value to hold the original tail, starts at head but will be updated to the last value in the linked list later
+    original_tail = head
+
+    # While loop that goes until end of linked list
+    while original_tail.next is not None:
+        # Update variable until at last value in linked list
+        original_tail = original_tail.next
+        # Update length till have full length of linked list
+        length += 1
+
+    # Now that have linked list length, use formula to discover what the offset/shift value should be
+    new_k = abs(k) % length
+    # If you get 0 then just return what was already sent in as there is no need to shift anything
+    if new_k == 0:
+        return head
+
+    # Calculate the position of the new tail while accounting for any special cases, such as negative k values
+    # IE: If k is positive, do length -k, else just use the new_k value as the position
+    new_tail_position = length - new_k if k > 0 else new_k
+    # Variable to store the future tail, will start at the head/first node as usual
+    new_tail = head
+
+    # For loop that goes until the new tail position is reached
+    for i in range(1, new_tail_position):
+        # Update new_tail variable until the new tail is reached
+        new_tail = new_tail.next    #
+
+    # Now just have to make sure the nodes and their pointers are swapped correctly
+
+    # The new head becomes equal to the pointer of the new tail
+    new_head = new_tail.next    # 3 --> 4 (new_head)
+    new_tail.next = None        # 3 --> None
+    original_tail.next = head   # 5 --> 0
+    print_linked_list(new_head)
+    return new_head             # 4 --> 5 --> 0 --> 1 --> 2 --> 3
+
+
+LL1 = LinkedList(0)
+LL1.next = LinkedList(1)
+LL1.next.next = LinkedList(2)
+LL1.next.next.next = LinkedList(3)
+LL1.next.next.next.next = LinkedList(4)
+LL1.next.next.next.next.next = LinkedList(5)
+
+# Function to print out the values
+def print_linked_list(item):
+    # base case
+    if item is None:
+        return
+    # Print current node
+    print(item.value)
+    # Print following node
+    print_linked_list(item.next)
+
+
+print_linked_list(LL1)
+print()
+
+shiftLinkedList(LL1, 2)
 
 
 
