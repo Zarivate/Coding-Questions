c1d67e5713bf890c5405e31e557bd307092a45dc
Zarivate <Zarivate1@gmail.com>
2023-03-21 23:58:53 -0500

    Implemented array jumps explanation and solution

--- Arrays/min_number_of_jumps.py
+++ Arrays/min_number_of_jumps.py
@@ -15,3 +15,40 @@
 # Answer:
 # 4 (Because can go from 3 --> 4 or 2 --> 2 or 3 --> 7 --> 3)
 
+# There are 2 ways to go about this, the second is far more clever but difficult to come up with but also more
+# efficient.
+
+# 1.
+# The first way involves creating another array of equal length to the input array where you would continuously
+# update how many jumps would be needed to reach the same point in the input array. The values in the "jump"
+# array would be updated by using two separate pointers, 1 for the input array and another for the "jumps" array.
+# The pointer for the input array would use value held within the current position to see if it could reach the
+# same point + 1 in the jumps array. IE:
+
+# array = [3, 4, 2, 1, 2, 3, 7, 1, 1, 1, 3]
+
+# Initialize every index as infinity besides the first position, as that would always take 0 jumps to get to
+# jumps = [0, inf, inf, inf, inf, inf, inf, inf, inf, inf, inf,]
+
+# Now
+
+
+# Space Time Complexity
+# O(n^2) time (Because have to continuously go over the same positions in the array when updating the jumps array)
+# O(n) space (Because would use up extra space of "n" size/input array size when making the jumps array)
+
+# Implementation
+def min_jumps(array):
+    # Make an array of equal size as input array filled with infinity values
+    jumps = [float("inf") for x in array]
+    # Initialize first value in jumps array as 0
+    jumps[0] = 0
+
+    # For loop to go through outer array
+    for i in range(1, len(array)):
+        # For loop to go through created jumps array
+        for j in range(0, i):
+            if array[j] >= i - j:
+                jumps[i] = min(jumps[j] + 1, jumps[i])
+    # Return last value in jumps array
+    return jumps[-1]
