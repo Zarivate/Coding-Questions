65279bae8908adba59b1dc4a4d9925ff8bbac39d
Zarivate <Zarivate1@gmail.com>
2023-03-24 23:59:38 -0500

    Created new directory for recursive based questions

--- Graphs/bipartite_two_colorable.py
+++ Graphs/bipartite_two_colorable.py
@@ -1,5 +1,15 @@
 # Note: Hadn't ever heard of the word "bipartite" until this question, still have lots to learn
 
 # Question:
-# Given a list of edges representing a graph with at least 1 node, wrtie a function that returns whether that
-# graph is two-colorable
+# Given a list of edges representing a graph with at least 1 node, write a function that returns True or False on
+# whether that graph is two-colorable/bipartite if every node can have 1 of 2 colors and has no edge connecting
+# any node of the same color.
+
+# Notes:
+# Graph is connected
+# Graph is unweighted, no distance value between nodes
+# Graph is undirected, meaning a node can go back and forth between any nodes it's connected to and vice versa.
+# There is no 1 way nodes essentially.
+# Graph may have self loops, meaning nodes that are connected to themselves forming a loop, if there are any self loops
+# in the graphs then it's automatically not two-colorable/returns False
+
---
+++ Recursive/Powerset.py
@@ -0,0 +1,4 @@
+# Question:
+# Write a function that takes in an array of unique elements (IE: integers, letters, etc), and returns it's powerset.
+
+# 
