1f14927d4b6e5fd469e1e8aa7cdf8a98cfd1cb4e
Zarivate <Zarivate1@gmail.com>
2023-04-02 23:58:30 -0500

    Added more to repeating character explanation

--- Strings/first_non_repeating.py
+++ Strings/first_non_repeating.py
@@ -8,6 +8,7 @@
 
 # Example:
 string_sample = "abcdcaf"
+
 # Answer: 1 (Because "b" was the first non-repeating character at index 1)
 
 # Optimal Space Time Complexity:
@@ -19,17 +20,38 @@ string_sample = "abcdcaf"
 # have more than 26 frequencies because that's the max number of lowercase English alphabet letters and
 # O(26) rounds down to O(1).
 
+# Explanation:
+# The main idea is to use a Hashtable, iterate through the string one time to fill up the table with the
+# characters and their frequencies, then go through the string again and whichever character has a value of
+# 1 in the table first, return that index. Else return -1.
+
+
 # Implementation:
 def first_non_repeating_character(string):
+    # Create a data structure that'll hold the characters and their frequencies
     char_frequencies = {}
 
+    # Iterate through string once
     for character in string:
+        # Fill up the data structure with the character alongside it's frequency
         char_frequencies[character] = char_frequencies.get(character, 0) + 1
 
+
+    # Iterate through string again
     for idx in range(len(string)):
+        # Get the current character in the string
         character = string[idx]
+        # Check to see if it has a value of 1 in the data structure
         if char_frequencies[character] == 1:
+            # If so, return its index
             return idx
     return -1
 
+
+# Bonus Un-optimal Solution:
+#
+
 print(first_non_repeating_character(string_sample))
+
+
+
