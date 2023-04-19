5d0ac511862263efaeb9de9859d3144f2ff255c5
Zarivate <Zarivate1@gmail.com>
2023-04-18 23:59:13 -0500

    Binary expression tree practice

--- BinaryTrees/Evaluate_expression_tree.py
+++ BinaryTrees/Evaluate_expression_tree.py
@@ -17,3 +17,56 @@
 # Bottom of the tree always evaluated first, regardless of operator. This is because of how
 # each operator also works as a grouping symbol
 
+# Example:
+class BinaryTree:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+# Visual example at end of program
+example_tree = BinaryTree(-1)
+example_tree.right = BinaryTree(-3)
+example_tree.right.left = BinaryTree(8)
+example_tree.right.right = BinaryTree(3)
+example_tree.left = BinaryTree(-2)
+example_tree.left.right = BinaryTree(2)
+example_tree.left.left = BinaryTree(-4)
+example_tree.left.left.left = BinaryTree(2)
+example_tree.left.left.right = BinaryTree(3)
+
+
+# Answer:
+# 6 (Because (((2*3) - 2) + (8/3)) = 6 - 2 + 2  = 6)
+
+
+# Optimal Space & Time Complexity:
+# O(n) time
+# O(h) space
+# n = number of nodes in Binary Tree
+# h = height of Binary Tree
+
+
+# Implementation:
+def evaluate_tree(tree):
+    if tree.value >= 0:
+        return tree.value
+
+    left_value = evaluate_tree(tree.left)
+    right_value = evaluate_tree(tree.right)
+
+    if tree.value == -1:
+        return left_value + right_value
+    if tree.value == -2:
+        return left_value - right_value
+    if tree.value == -3:
+        return int(left_value / right_value)
+
+    return left_value * right_value
+
+
+print(evaluate_tree(example_tree))
+
+
+# In depth breakdown:
