c32c7130b4482f739900a97aabe4adf176601bd7
Zarivate <Zarivate1@gmail.com>
2023-05-15 23:57:39 -0500

    Implemented array missing values question

---
+++ Arrays/missing_numbers.py
@@ -0,0 +1,26 @@
+# Question
+
+# Write a function that takes in an unordered list of unique integers called "nums" in the range of [1, n], where
+# n = len(nums + 2), and returns a new list of the 2 missing numbers sorted numerically.
+
+
+# Example:
+
+nums_example = [1, 4, 3]
+
+
+# Answer
+# [2, 5]
+# len(nums_example) = 3, meaning the numbers that could be in the array range from 1-5. The example array is missing
+# the 2 and 5 from the given range so return those numbers in an array in ascending order.
+
+
+# Optimal Space & Time Complexity:
+# O(n) time, because will have to iterate through the array
+# O(1) space, this is because the new array we create will always have 2 integer values in it. Meaning it will take up
+# a total space of O(2) that can be reduced down to O(1) since.
+
+
+# Explanation:
+# The main idea to realize here is how 
+
