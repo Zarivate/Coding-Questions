c886b9a34d748b5d5caccf64f032702c76d51af9
Zarivate <Zarivate1@gmail.com>
2023-05-31 23:57:58 -0500

    Implemnted pseudo sequel question to previous one

---
+++ Extra/Minimum_number_coins_for_change.py
@@ -0,0 +1,18 @@
+# Question:
+
+# A pseudo sequel to the number of ways to make change question, in this question you're also given an array of positive
+# integers representing coin values and a single non-negative integer "n" representing a target value. Write a function
+# that returns the smallest number of coins needed to make change for the target "n"
+
+# Notes:
+# Each coin denomination allows for an unlimited number of uses for that coin
+# If not possible to reach target "n" from given coins, return -1
+
+# Example:
+n = 7
+example_coins = [1, 5, 10]
+
+# Answer
+# 3 
+
+
