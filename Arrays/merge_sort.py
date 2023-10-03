8ef31ddb202ae463ae590e9eb760b33a5151e24b
Zarivate <Zarivate1@gmail.com>
2023-10-02 23:59:35 -0500

    Lost merge sort file somehow so had to redo, is fine tho

---
+++ Arrays/merge_sort.py
@@ -0,0 +1,20 @@
+# Question:
+
+# Write a function that takes in an array of integers and returns a sorted version of that array using the Merge Sort
+# algorithm. 
+
+# Example:
+example_array = [8, 5, 2, 9, 5, 6, 3]
+
+# Answer:
+# example_array = [2, 3, 5, 5, 8, 8, 9]
+
+# Optimal Space & Time Complexity:
+# Best, Worst, and Average Case:
+# O(nlog(n)) time
+# O(n) space 
+# n = length of input array
+
+
+# Explanation:
+# To start, what 
