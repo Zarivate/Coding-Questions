d5c1cbb5bf70dd8464ac60ee3cc5a2ec8d850910
Zarivate <Zarivate1@gmail.com>
2023-12-11 23:59:37 -0600

    Dijkstras algorithm time

---
+++ Extra and Dynamic Programming/Dijkstra.py
@@ -0,0 +1,17 @@
+# Question
+
+# Given a integer "start" value, and a list of "edges" comprised of pairs of integers.
+
+
+
+# Example:
+start = 0
+
+edges = [
+    [[1,7]],
+    [[2,6], [3, 20], [4, 3]],
+    [[3, 14]],
+    [[4, 2]],
+    [],
+    [],
+]
