014fbcdca9ab533b2c3fbd788c9e6808d4eb5f26
Zarivate <Zarivate1@gmail.com>
2024-03-21 23:59:00 -0500

    All possible IP addresses question

---
+++ Strings/validIP.py
@@ -0,0 +1,9 @@
+# Question:
+# Given a string of length 12 or smaller with only digits. Write a function that returns all possible
+# IP addresses that can be created by inserting three periods (.) within the string. All addresses 
+# returned should be in string format in no particular order. If no valid IPs, return an empty list.
+
+# Notes:
+# Every digit created from insertion of the periods must be in range between 0-255 (inclusive).
+# Digits within input string will only ever be positive.
+# Also this question is honestly really interesting due to it's optimal space and time complexity.
