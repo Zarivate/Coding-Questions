d8d1f76f9b9d19a1c8ee050683d07b4835050624
Zarivate <Zarivate1@gmail.com>
2024-04-05 23:59:09 -0500

    Greedy algorithm questions now

---
+++ Extra and Dynamic Programming/Valid_starting_city.py
@@ -0,0 +1,10 @@
+# Question:
+
+# Given 2 arrays, one representing distances between cities and another the fuel/gallons that can be obtained at those cities 
+# alongside a mpg, return the starting city index that allows you to start at and travel all the way around the cities on the
+# exact amount of fuel needed.
+
+# Notes:
+# Can assume will always be given at least 2 cities.
+# Fuel tank will always start out empty.
+# Mpg will always be positive.
